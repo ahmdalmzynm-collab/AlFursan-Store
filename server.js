9a3628ea75b3c36bbd39dc15246f5eea2f95d78a
ahmdalmzynm-collab <ahmdalmzynm@gmail.com>
2026-02-01 07:29:51 -0800

    Update server.js

--- server.js
+++ server.js
@@ -2,19 +2,25 @@ const express = require('express');
 const path = require('path');
 const app = express();
 
-// كود جوجل - ده أهم حاجة عشان التوثيق
+// إعدادات المحرك لضمان الوصول للملفات في بيئة Vercel
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, 'views'));
+app.use(express.static(path.join(__dirname, 'public')));
+
+// ملف توثيق جوجل (ثابت ومضمون)
 app.get('/googlef2bedd81991940c6.html', (req, res) => {
     res.send('google-site-verification: googlef2bedd81991940c6.html');
 });
-});
 
-// عشان الموقع ميضربش لو الفولدرات تاهت
-app.set('view engine', 'ejs');
-app.set('views', path.join(__dirname, 'views'));
-
+// الصفحة الرئيسية (Index)
 app.get('/', (req, res) => {
-    res.send('<h1>مركز الفرسان شغال بنجاح!</h1><p>جاري تحديث المنتجات...</p>');
+    // مصفوفة المنتجات - دي اللي هتعدل فيها بعدين براحتك
+    const products = [
+        { id: 1, name: "منتج رقم 1", price: "100", image: "https://via.placeholder.com/150" },
+        { id: 2, name: "منتج رقم 2", price: "250", image: "https://via.placeholder.com/150" }
+    ];
+    res.render('index', { products });
 });
 
+// تصدير التطبيق - الطريقة الأكيدة لبيئة الـ Serverless
 module.exports = app;
-
